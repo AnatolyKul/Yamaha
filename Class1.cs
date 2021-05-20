2bcde8c6da76b25d13e676d40c641814493fa0cb
AnatolyKul <84135820+AnatolyKul@users.noreply.github.com>
2021-05-20 17:00:12 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Motosycle
+    {
+        public Motosycle(string v1, string v2, string v3, string v4)
+        {
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+            V4 = v4;
+        }
+
+        public string V1 { get; }
+        public string V2 { get; }
+        public string V3 { get; }
+        public string V4 { get; }
+        internal void name (string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void color(string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void model(string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void speed(string v)
+        {
+            Console.WriteLine(V1 + ":" + v);
+        }
+        internal void move (Motosycle v)
+        {
+            Console.WriteLine("Yamaha move");
+        }
+        internal void stop(Motosycle v)
+        {
+            Console.WriteLine("Yamaha stop");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motosycle Yamaha = new Motosycle("Yamaha", "Green", "R-1", "400 km/h");
+            Yamaha.name("Yamaha");
+            Yamaha.color("Green");
+            Yamaha.model("R-1");
+            Yamaha.speed("400 km/h");
+            Yamaha.move(Yamaha);
+            Yamaha.stop(Yamaha);
+        }
+    }
+}
